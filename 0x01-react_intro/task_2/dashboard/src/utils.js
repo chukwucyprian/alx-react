2d364ae399f2f9ebbd8760b9378a5b86e600e4ed
chukwucyprian <chukwucyprian262@gmail.com>
2024-02-02 12:39:19 +0100

    correction of output

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,12 +1,12 @@
-// Create a function named getFullYear that will return the current year
 export function getFullYear() {
     return new Date().getFullYear();
 }
 
-// Create a function named getFooterCopy that will return the following string
 export function getFooterCopy(isIndex) {
-    if (isIndex) {
-        return 'Holberton School';
-    }
-    return 'Holberton School main dashboard';
+    return isIndex ? 'Holberton school' : 'Holberton School main dashboard';
+}
+
+export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+    
 }
