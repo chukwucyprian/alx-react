14cc7f21b466e76db965b01fda4dce4fdb4be7c1
chukwucyprian <chukwucyprian262@gmail.com>
2024-03-17 20:10:36 +0100

    correction

--- 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/actions/notificationActionTypes.js
+++ 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/actions/notificationActionTypes.js
@@ -1,3 +1,4 @@
+
 export const MARK_AS_READ = "MARK_AS_READ";
 
 export const SET_TYPE_FILTER = "SET_TYPE_FILTER";
@@ -6,3 +7,7 @@ export const NotificationTypeFilters = {
   DEFAULT: "DEFAULT",
   URGENT: "URGENT",
 };
+
+export const FETCH_NOTIFICATIONS_SUCCESS = "FETCH_NOTIFICATIONS_SUCCESS";
+
+export const SET_LOADING_STATE = "SET_LOADING_STATE";
